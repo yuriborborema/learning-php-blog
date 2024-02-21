e005857149a4d9be24c1a80c0ad51ad7c07da149
Yuri <yuriborborema10@gmail.com>
2024-02-21 15:49:17 -0300

    feat: index and imports

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+    include_once "helpers/url.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Blog Codar</title>
+
+     <!-- ESTILOS DO PROJETO -->
+    <link rel="stylesheet" href="<?= $BASE_URL ?>/css/style.css">
+
+    <!-- GOOGLE FONTS -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Kode+Mono:wght@400..700&display=swap" rel="stylesheet">
+</head>
+<body>
+    <h2>
+        Hello World!
+    </h2>
+</body>
+</html>
